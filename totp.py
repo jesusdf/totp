3b109966d37f4d5d6c4b4717e6414824b1ef402e
James Lin <jamesdlin@gmail.com>
2021-11-25 23:10:36 -0800

    Make a less minimal version of MinTOTP
    Make a less minimal version that improves on some usability aspects:
    * Prompts for the TOTP key.
    * Uses Python's `getpass` to read the TOTP key without echoing it.
    * Ignores all spaces in the TOTP key.
    * Reads only one TOTP key at a time.
    * Avoids printing Python backtraces if interrupted with Ctrl+C or if
      an invalid base32 character is entered for the TOTP key.
    
    Also remove most of the auxiliary files (such as build and test
    files) that I don't care about.

--- totp.py
+++ totp.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
+"""
+Generates a TOTP code.
+
+Based on MinTOTP by Susam Pal.
+"""
+# Forked from <https://github.com/susam/mintotp>.
+
 
 import base64
+import binascii
+import getpass
 import hmac
 import struct
 import sys
@@ -20,11 +29,19 @@ def totp(key, time_step=30, digits=6, digest='sha1'):
     return hotp(key, int(time.time() / time_step), digits, digest)
 
 
+def normalize_key(key):
+    return "".join((c for c in key if not c.isspace()))
+
+
 def main():
-    args = [int(x) if x.isdigit() else x for x in sys.argv[1:]]
-    for key in sys.stdin:
-        print(totp(key.strip(), *args))
+    key = getpass.getpass(prompt="TOTP key: ")
+    print(totp(normalize_key(key)))
 
 
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    try:
+        main()
+    except binascii.Error as e:
+        print(e, file=sys.stderr)
+    except KeyboardInterrupt:
+        sys.exit(1)
